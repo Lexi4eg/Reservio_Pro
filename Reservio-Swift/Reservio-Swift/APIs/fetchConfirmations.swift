84b27bc0342ba2046c80aecad5995a698cf91004
Lexi4eg <felix.prattes@gmail.com>
2024-09-27 09:46:49 +0200

    New Fetchconfirmation methode for talking to the endpoint from the swift ui frontend

---
+++ Reservio-Swift/Reservio-Swift/APIs/fetchConfirmations.swift
@@ -0,0 +1,52 @@
+//
+//  fetchConfirmations.swift
+//  Reservio-Swift
+//
+//  Created by Felix Prattes on 27.09.24.
+//
+
+import Foundation
+
+func fetchConfirmations(firstname: String, lastname: String) async -> [Confirmation] {
+    
+    
+
+    guard var urlComponents = URLComponents(string: "http://localhost:4567/getConfirmationsByName") else {
+        print("Error: Invalid URL")
+        return []
+    }
+    
+    urlComponents.queryItems = [
+        URLQueryItem(name: "firstname", value: firstname),
+        URLQueryItem(name: "lastname", value: lastname)
+
+    ]
+    
+    guard let url = urlComponents.url else {
+        print("Error: Invalid URL components")
+        return []
+    }
+
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+
+    do {
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+            print("Available Confirmations received successfully")
+            if let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: []) as? [Confirmation] {
+                return jsonResponse
+            } else {
+                print("Error parsing JSON response")
+                return []
+            }
+        } else {
+            print("Error checking table times: \(String(decoding: data, as: UTF8.self))")
+            return []
+        }
+    } catch {
+        print("Error sending request: \(error)")
+        return []
+    }
+}
