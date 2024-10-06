135d6b2e985278d585469999bb8816993d8863a0
Lexi4eg <felix.prattes@gmail.com>
2024-10-06 18:00:59 +0200

    New Patch Procesing service

---
+++ DataAnalyticsPipeline/PatchProcessingService.py
@@ -0,0 +1,54 @@
+import time
+import matplotlib.pyplot as plt
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+engine = create_engine('postgresql+psycopg2://postgres:password@localhost/postgres')
+
+
+
+
+
+class PatchProcessingService ():
+    def __init__(self):
+        query = 'SELECT * FROM reservations'
+        self.df = pd.read_sql(query, engine)
+        self.df['date'] = pd.to_datetime(self.df['date'])
+        self.df['peopleCount'] = self.df['peopleCount'].astype(int)
+
+    def plot_data(self):
+        print("Plotting data")
+        print(self.df[['date', 'peopleCount']])
+
+        plt.plot(self.df['date'], self.df['peopleCount'])
+        plt.xlabel('Date')
+        plt.ylabel('People Count')
+        plt.title('People Count Over Time')
+        plt.show()
+
+    def refresh_data(self):
+        query = 'SELECT * FROM reservations'
+        self.df = pd.read_sql(query, engine)
+        self.df['date'] = pd.to_datetime(self.df['date'])
+        self.df['peopleCount'] = self.df['peopleCount'].astype(int)
+        return self.df
+
+
+    def patchService(self):
+        while True:
+            print("Patch Service")
+            self.refresh_data()
+            self.plot_data()
+            print(self.df)
+            time.sleep(60)
+            return self.df
+
+
+
+if __name__ == '__main__':
+    patch = PatchProcessingService()
+    patch.patchService()
+
+
+
