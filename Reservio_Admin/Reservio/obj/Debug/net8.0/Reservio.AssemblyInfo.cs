ffbdebb103051833eeddd9da58bff5911c2e406d
skollegger <sabinekollegger2@gmail.com>
2024-11-19 11:39:58 +0100

    cs
    csa

--- Reservio_Admin/Reservio/obj/Debug/net8.0/Reservio.AssemblyInfo.cs
+++ Reservio_Admin/Reservio/obj/Debug/net8.0/Reservio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reservio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7cf8d0a7a8aa44e7a0659528391603e69f18331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3983a4292bff69e2fb94f07eedcc96643a99dd8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reservio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reservio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
